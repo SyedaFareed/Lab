4f82c86c0973ece0affe297c2d82ec7f0a455924
SyedaFareed <14besesfareed@seecs.edu.pk>
2017-07-12 21:49:09 +0500

    Add files via upload

---
+++ Allocator.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab2;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+/**
+ *
+ * @author Syeda Fareed
+ */
+public class Allocator {
+    
+    ArrayList<ArrayList> friends;
+
+    public Allocator() {
+        this.friends = new ArrayList<ArrayList>;
+    }
+    
+}
---
+++ Lab2.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Syeda Fareed
+ */
+public class Lab2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("Please enter 1 for Reservation, and 2 for Impromptu");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.next();
+        
+    }
+    
+}
